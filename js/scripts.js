d0a9ce82ab81e92451eb42b2b2dbc40f221fb63c
Lois Choi <loisch22@gmail.com>
2017-07-06 18:47:04 -0700

    add jquery/JS functions, defined var

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-  $("#appointment form").submit(function() {
+$(document).ready(function() {
+  $("button#submit").click(function(event) {
+    event.preventDefault();
+    var nameInput = $("#name").val();
+    var dateInput = $("#date").val();
+    var whatToDoInput = $("#whatToDo").val();
+    var timeInInput = $("#timeIn").val();
+    var timeOutInput = $("#timeOut").val();
 
+    $(".nameInput").text(nameInput);
+    $(".dateInput").text(dateInput);
+    $(".whatToDoInput").text(whatToDoInput);
+    $(".timeInInput").text(timeInInput);
+    $(".timeOutInput").text(timeOutInput);
+    $("#receipt").show();
   });
 });
